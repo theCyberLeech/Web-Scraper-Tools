40024e57dd970b1a48c1589f2dcf820e1226d906
theCyberLeech <102625690+theCyberLeech@users.noreply.github.com>
2023-08-26 14:25:15 -0700

    Update NewNVDNistScraper.py
    The updates to the nvdlib Python library broke the script.  The updates to this script fix the issues that were caused by the updates. The results are now outputted to a text file that is formatted in a way that it will allow it to be uploaded to an Excel spreadsheet, making the data manageable and workable! 
    
    Vulnerabilities will now be able to be sorted!

--- NewNVDNistScraper.py
+++ NewNVDNistScraper.py
@@ -16,133 +16,106 @@ from art import *
 date = datetime.now().strftime('%Y_%m_%d-%I:%M:%S_%p')
 original_stdout = sys.stdout
 
-
-
 def searchByCVE():
-	input_string = input('Which CVE IDs do you want to look up?\n')
-	CVEID = input_string.split()
-	with open('CVESearch' + date + '.txt', 'w') as f:
-		sys.stdout = f
-		for i in CVEID:
-			try:
-				r = nvdlib.getCVE(i)
-			except LookupError:
-				print(i + ' does not exist.')
-				continue
-			print('\nCVE ID: ' + i)
-			PubDate = r.publishedDate
-			timeParsed = zulu.parse(PubDate)
-			print('Published Date: ', end='')
-			print(timeParsed)
-			try:
-				print('v3 Vector: ' + r.v3vector)
-			except AttributeError:
-				print('v3 Vector Score not available')
-			print('Attack Vector: ' + r.impact.baseMetricV3.cvssV3.attackVector)
-			print('Attack Complexity: ' + r.impact.baseMetricV3.cvssV3.attackComplexity)
-			print('Privileges Required: ' + r.impact.baseMetricV3.cvssV3.privilegesRequired)
-			print('User Interaction: ' + r.impact.baseMetricV3.cvssV3.userInteraction)
-			print('CWE ID: ' + r.cve.problemtype.problemtype_data[0].description[0].value)
-			print('Exploitability: ', end='')
-			try:
-				print(r.v3exploitability)
-			except AttributeError:
-				print('Not available')
-			print('V3 Score: ', end='')
-			print(r.score[1], end=' - ')
-			print(r.score[2])
-			print('\nDescription: ' + r.cve.description.description_data[0].value + '\n')
-			print('\nReferences: ')
-			count = 0
-			print('Nist URL: ' + str(r.url))
-			References = r.cve.references.reference_data
-			for eachURL in References:
-				length = range(len(References))
-				Refs = r.cve.references.reference_data[count].url
-				print(Refs)
-				count += 1
-			print('------------------------------------------------------------\n')
-		sys.stdout = original_stdout
-	f.close()
-	whatNext()
-	Menu()
+        input_string = input('Which CVE IDs do you want to look up?\n')
+        CVEID = input_string.split()
+
+        with open('CVESearch' + date + '.txt', 'w') as f:
+                sys.stdout = f
+                print('CVE ID;; CWE ID;; Affected CPE IDs;;V31 Severity;; Description;; References') 
+                for i in CVEID:
+                        try:
+#                               r = nvdlib.searchCVE(cveId=i, key='XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX') # << Place your API key here
+                                r = nvdlib.searchCVE(cveId=i)
+                        except LookupError:
+                                print(i + ' does not exist.;;', end= '\n')
+                                continue
+                        print(i + ';;', end=' ')
+                        print(r[0].published + ';;', end=' ')
+                        print(str(r[0].weaknesses[0].description[0].value) + ';;', end=' ')
+                        count = 0
+                        cpeAffected = r[0].cpe
+                        for e in cpeAffected:
+                                length = range(len(cpeAffected))
+                                cpes = r[0].cpe[count].criteria
+                                print(cpes, end=', ')
+                                count += 1
+                        print(end=';; ')
+                        print(str(r[0].score[1]) + ' ' + r[0].score[2], end=';; ')
+                        print(r[0].descriptions[0].value, end=';; ')
+                        count = 0
+                        print(str(r[0].url), end=', ')
+                        References = r[0].references
+                        for eachURL in References:
+                                length = range(len(References))
+                                Refs = r[0].references[count].url
+                                print(Refs, end=', ')
+                                count += 1
+                        print(end='\n')
+                sys.stdout = original_stdout
+        f.close()
+        whatNext()
+        Menu()
 
 
 
 
 def searchByKeyword():
-	inputString = input('What service || Software do you want to search for? \n')
-	r = nvdlib.searchCPE(keyword = inputString , limit = 100)
-	with open(inputString + date + '.txt', 'w') as f:
-		sys.stdout = f
-		for eachCPE in r:
-			print('\nTitle: ', end=eachCPE.title)
-			print('\nCPE Name: ' + eachCPE.name)
-			References = eachCPE.refs
-			count = 0
-			print('\n\nReferences:')
-			for eachURL in References:
-				print(References[count].ref, end=' Type: ')
-				print(References[count].type)
-				count += 1
-			print('-------------------------------------------------------\n')
-		sys.stdout = original_stdout
-	f.close()
-	whatNext()
-	Menu()
+        inputString = input('What service || Software do you want to search for? \n')
+#       r = nvdlib.searchCPE(keywordSearch=inputString, key='xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx') # << Place your API key here
+        r = nvdlib.searchCPE(keywordSearch=inputString)
+        with open(inputString + date + '.txt', 'w') as f:
+                sys.stdout = f
+                print('Keyword Search; Published; Title; Deprecated?; CPE Name; CPE ID', end='\n')
+                for i in r:
+                        print(inputString, end='; ')
+                        print(i.created, end='; ')
+                        print(str(i.titles[0].title), end='; ')
+                        print(i.deprecated, end='; ')
+                        print(i.cpeName, end='; ')
+                        print(i.cpeNameId, end='\n')
+                sys.stdout = original_stdout
+        f.close()
+        whatNext()
+        Menu()
 
 
 
 def searchByCPE():
-	CPE = input(''' 
+        CPE = input(''' 
                                            _____
                                             | |
         What is the cpe that you you seek? <0.o>
 
         Starting with "cpe:" enter the cpe that you wish to conduct your search with:
-	Example: cpe:2.3:a:openbsd:openssh:7.2:p2:*:*:*:*:*:*\n
+        Example: cpe:2.3:a:openbsd:openssh:7.2:p2:*:*:*:*:*:*\n
 ''')
-#       CPERegEx()
-	try:
-		vulns = nvdlib.searchCVE(cpeName=CPE, exactMatch=True)
-		with open(CPE + '.txt', 'w') as f:
-			sys.stdout = f
-			for i in vulns:
-				print('CVE ID: ' + i.id)
-				print('Severity: ' +  str(i.score[1]) + ' - ' + str(i.score[2]))
-				print('NVD URL: ' +  i.url)
-				print('\nDescription: ' + i.cve.description.description_data[0].value + '\n')
-				try:
-        	        	        print('v3 Vector: ' + i.v3vector)
-				except AttributeError:
-					print('v3 Vector Score not available')
-				print('Attack Vector: ' + i.impact.baseMetricV3.cvssV3.attackVector)
-				print('Attack Complexity: ' + i.impact.baseMetricV3.cvssV3.attackComplexity)
-				print('Privileges Required: ' + i.impact.baseMetricV3.cvssV3.privilegesRequired)
-				print('User Interaction: ' + i.impact.baseMetricV3.cvssV3.userInteraction)
-				print('CWE ID: ' + i.cve.problemtype.problemtype_data[0].description[0].value)
-				print('Nist URL: ' + str(i.url))
-				print('Exploitability: ', end='')
-				try:
-					print(i.v3exploitability)
-				except AttributeError:
-					print('Not available')
-				print('V3 Score: ', end='')
-				print(i.score[1], end=' - ')
-				print(i.score[2])
-				print('\nReferences: ')
-				count = 0
-				References = i.cve.references.reference_data
-				for eachURL in References:
-					Refs = i.cve.references.reference_data[count].url
-					print(Refs)
-					count += 1
-				print('---------------------------------------\n')
-			sys.stdout = original_stdout
-		f.close()
-	except LookupError:
-		print(CPE + ' does not exist. Double-check your input.')
-	whatNext()
+        try:
+#               vulns = nvdlib.searchCVE(cpeName=CPE, key='xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx') # << Place your API key here
+                vulns = nvdlib.searchCVE(cpeName=CPE)
+                with open(CPE + '.txt', 'w') as f:
+                        sys.stdout = f
+                        print('CVE ID;; Published;; Severity;; CWE ID;; Description;; NVD URL;; References', end='\n')
+                        for i in vulns:
+                                print(i.id + ';;', end=' ')
+                                print(i.published + ';;', end=' ')
+                                print(str(i.score[1]) + ' - ' + str(i.score[2]) + ';;', end=' ')
+                                print(i.cwe[0].value + ';;', end=' ')
+                                print(i.descriptions[0].value + ';;', end=' ')
+                                count = 0
+                                References = i.references
+                                print(i.url, end=';; ')
+                                for URL in References:
+                                        length = range(len(References))
+                                        Refs = i.references[count].url
+                                        print(Refs, end=', ')
+                                        count += 1
+                                print(end='\n')
+                        sys.stdout = original_stdout
+                f.close()
+        except LookupError:
+                print(CPE + ' does not exist. Double-check your input.')
+        whatNext()
 
 
 
@@ -160,42 +133,45 @@ def whatNext():
         Menu()
 
 
-
+def Banner():
+        NVDNistBanner = text2art('NVDNISTSCRAPEr.py', font='rnd-small')
+        print(NVDNistBanner)
 
 
 
 
 def Menu():
-	print('''
-	\n\n
-				Welcome to the NewNVDNistScrapper.py!
-	This tool can be used to grab information on known vulnerabilities inside the NVD framework.
+        print('''
+        \n\n
+                                Welcome to the NewNVDNistScrapper[0].py!
+        This tool can be used to grab information on known vulnerabilities inside the NVD framework.
 
-					Created By Ana De Jesus
+                                        Created By Ana De Jesus
 \n\n''')
-	menuInput = input('''
-	(1) Search by CVE
-	(2) Search for CPE by Keyword
-	(3) Search by CPE Name
-	(4) Exit Program
+        menuInput = input('''
+        (1) Search by CVE
+        (2) Search for CPE by Keyword
+        (3) Search by CPE Name
+        (4) Exit Program
 
 Make your selection: \n''')
-	if menuInput == '1':
-		searchByCVE()
-	elif menuInput == '2':
-		searchByKeyword()
-	elif menuInput =='3':
-		searchByCPE()
-	elif menuInput == '4':
-		print('Good-bye.....')
-		sys.exit()
-	else:
-		print('Bad input, try again. Do not use spaces. Enter 1, 2, 3 or 4.')
-	Menu()
+        if menuInput == '1':
+                searchByCVE()
+        elif menuInput == '2':
+                searchByKeyword()
+        elif menuInput =='3':
+                searchByCPE()
+        elif menuInput == '4':
+                print('Good-bye.....')
+                sys.exit()
+        elif menuInput == '5':
+                Banner()
+        else:
+                print('Bad input, try again. Do not use spaces. Enter 1, 2, 3 or 4.')
+        Menu()
 
-NVDNistBanner = text2art('NVDNISTSCRAPER.py', font='rnd-small')
+NVDNistBanner = text2art('NVDNISTSCRAPEr.py', font='rnd-small')
 
 
-print(NVDNistBanner)
+Banner()
 Menu()
-
